d45018f220c0e4c3d29d4158014be94ee75cc932
MonikaSharma27 <monikasharma279826@gmail.com>
2024-06-05 12:59:32 +0530

    completed

--- app.js
+++ app.js
@@ -7,46 +7,51 @@ let page=1;
 
 
 const fetchImages= async (query,pageNo)=>{
+
+    try{
+
     if(pageNo === 1){
         imagesContainer.innerHTML = '';
-    }
-    
+    } 
     const url = `https://api.unsplash.com/search/photos?query= ${query}&per_page=28&page=${pageNo}&client_id=${accessKey}`;
-
     const p = await fetch(url);
     const data = await p.json();
-    
+    console.log(data)
 
-   if (data.results.length > 0){
-    data.results.forEach(photo => {
-        const imageElement = document.createElement("div")
-        imageElement.classList.add("imageDiv")
-        imageElement.innerHTML= `<img src = "${photo.urls.regular}"/>`
-    
-    
-    const overlayElement = document.createElement("div");
-    overlayElement.classList.add("overlay");
-    
-    const overlayText = document.createElement("h3")
-    overlayText.innerText= `${photo.alt_description}`;
-    
-    overlayElement.appendChild(overlayText);
-    imageElement.appendChild(overlayElement);
-        imagesContainer.appendChild(imageElement);
-       });
-    
-    if(data.total_pages === pageNo){
-        loadMoreBtn.style.display ="none";
-    }else{
-        loadMoreBtn.style.display="block";
+    if(data.results.length > 0){
+        data.results.forEach(photo => {
+            const imageElement = document.createElement("div");
+            imageElement.classList.add("imageDiv");
+            imageElement.innerHTML= `<img src= "${photo.urls.regular}"/>`;
+            
+            const overlayElement = document.createElement("div");
+            overlayElement.classList.add("overlay");
+            
+            const overlayText = document.createElement("h3");
+            overlayText.innerText=`${photo.alt_description}`;
+            
+            overlayElement.appendChild(overlayText)
+            imageElement.appendChild(overlayElement);
+            imagesContainer.appendChild(imageElement);
+                    
+                });
+            
+                if (data.total_pages === pageNo){
+                    loadMoreBtn.style.display="none";
+            
+                }
+                else{
+                    loadMoreBtn.style.display="block";
+                }
     }
-   }else{
-    imagesContainer.innerHTML= `<h2>No image found..</h2>`
-     loadMoreBtn.style.display ="none";
+   else{
+    imagesContainer.innerHTML= `<h2>No image found...</h2>`
    }
-
-   
-
+}
+ catch(error){
+    imagesContainer.innerHTML= `<h2>failed to fetch images. please try again later.</h2>`
+    loadMoreBtn.style.display ="none";
+}
 }
 
 
